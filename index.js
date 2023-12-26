517abcf789d9d2a6d87e85286ec29a7d13f09bf9
AhmedJerebicanin123 <jerebicaninahmed10@gmail.com>
2023-12-26 20:49:00 +0100

    js

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log('Ovo je nas prvi cas !');
+
+var ovoJeNumber = 23;
+var ovoJeBoolean = true;
+var ovoJeString = 'Ahmed';
+var ovoJeUndefined = undefined;
+var ovoJeNull = null;
+
+var ovoJeArray = ['d', 2, {}];
+var ovoJeObject  = {
+    ime : 'Ahmed',
+    prezime : 'Jerebicanin',
+    godine : 13
+}
+function test(){
+
+};
+
+console.log(ovoJeNumber)
