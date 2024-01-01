df1acf43b8d798f3a6a2c9c45bd908b8a0cfa598
AhmedJerebicanin123 <jerebicaninahmed10@gmail.com>
2024-01-01 17:40:50 +0100

    js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log('Ovo je nas prvi cas !');
-
 var ovoJeNumber = 23;
 var ovoJeBoolean = true;
 var ovoJeString = 'Ahmed';
@@ -12,8 +10,56 @@ var ovoJeObject  = {
     prezime : 'Jerebicanin',
     godine : 13
 }
-function test(){
+function test(e){
+    console.log(e)
+}
 
-};
+// test(5)
 
-console.log(ovoJeNumber)
+// console.log(ovoJeUndefined);
+// console.log(ovoJeNumber)
+// console.log(ovoJeArray)
+// console.log(ovoJeObject)
+// console.log(ovoJeNull);
+
+let dodeliVrednost = 23;
+let plus = 2+2;
+let minus = 2-2;
+let mnozenje = 2*2;
+let deljenje = 2/2;
+let number = 2;
+let string = '2';
+
+let test1 = (number == string) || false;
+// console.log(!test1);
+
+let test2 = 2>=2                                                            
+// console.log(test2);
+
+let mojNovacanik = 100;
+
+const kafa = 10;
+const sok = 20;
+const cigare = 250;
+
+console.log(mojNovacanik >= kafa);
+mojNovacanik = mojNovacanik - kafa; 
+console.log(mojNovacanik);
+console.log(mojNovacanik >= (kafa, sok));
+mojNovacanik = mojNovacanik - sok
+console.log(mojNovacanik);
+console.log(mojNovacanik >= cigare);
+
+
+//             ________________
+//          __|                |__
+//       __|     ____     ____    |__
+//    __|       |    |   |    |      |__
+//   |          |____|   |____|         |
+//   |                                  | 
+//   |    |__                     __|   |
+//   |       |__                __|     |      
+//   |__        |______________|      __|
+//      |__                        __| 
+//         |__                  __| 
+//            |________________|
